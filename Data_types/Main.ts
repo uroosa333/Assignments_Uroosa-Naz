fe090cae3327006e4913b539d8b93730985736c4
uroosa333 <160825074+uroosa333@users.noreply.github.com>
2024-03-11 21:45:27 +0500

    Add files via upload

---
+++ Data_types/Main.ts
@@ -0,0 +1,40 @@
+// Datatypes,
+// 1.String
+
+let personName: string = "Muntaha" ;
+console.log("Have a good day," + personName + "!");
+
+// 2.Number
+
+let personAge: number = 5 ;
+let finalage: number = personAge + 1 ;
+console.log("personage," + finalage);
+
+// 3.Boolean
+
+let username: string = "Muntaha" ;
+let age: number = 5 ;
+let isStudent: boolean = true ;
+console.log(isStudent);
+
+// 4.Null
+
+let value: null = null ;
+console.log(null);
+
+// 5.Undefined
+
+let randomvalue: undefined = undefined ;
+console.log(randomvalue) ;
+
+// 6.Symbol
+
+let myName = Symbol("Muntaha") ;
+console.log(myName) ;
+
+// 7.Bigint
+
+let num1 = BigInt (1234567890);                             
+let num2 = BigInt( 12345678901234567890) ;                                  
+console.log(num1 * num2) ;
+
