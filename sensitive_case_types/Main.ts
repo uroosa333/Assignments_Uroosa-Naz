53fe4137f440174b5dc25145361025c424fe1b10
uroosa333 <160825074+uroosa333@users.noreply.github.com>
2024-03-11 21:33:11 +0500

    Add files via upload

---
+++ sensitive_case_types/Main.ts
@@ -0,0 +1,40 @@
+// Sensitive Case
+// 1.camelCase
+
+let userName = "Uroosa";
+console.log(userName);
+
+// 2.Snake_case
+
+let is_user_exist = true;
+console.log(is_user_exist);
+
+// 3.PascalCase
+
+let PersonAge = 5;
+console.log(PersonAge);
+
+// Data_Structure 
+// 1.Arrays []
+
+// (a).String[]
+
+let students: string[] = ["Uroosa","Arifa","Ayesa"];
+console.log(students[0] , students[2]);
+
+// (b).Boolean[]
+
+let personName: boolean[] = [true,false,false,true,true,true,false];
+console.log(personName[1], personName[6]);
+
+// (c).Arrays Update[]
+
+let fruits = ["Apple","Mango","Banana","Orange"];
+fruits[1] = "Grapes";
+console.log(fruits);
+
+// (d). Number[]
+
+let listNumer: number[] = [1,2,3,4,5];
+console.log(listNumer[0] , listNumer[4]);
+
